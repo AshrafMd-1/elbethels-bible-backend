c47ed33b542d13b6334bc5ecf7726f9f00202436
Ashraf Mohammed <ashrafmd0506@gmail.com>
2025-01-31 11:57:16 +0530

    add mongodb store

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const statusRoutes = require("./routes/statusRoutes");
 const authRoutes = require("./routes/authRoutes");
 const fetchRoutes = require("./routes/fetchRoutes");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
@@ -10,14 +12,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: "session",
+});
+
+store.on("error", function (error) {
+  console.log(error);
+});
+
 app.use(
-  require("express-session")({
+  session({
     secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: false,
+      httpOnly: false,
     },
     resave: false,
     saveUninitialized: true,
+    store: store,
   }),
 );
 
